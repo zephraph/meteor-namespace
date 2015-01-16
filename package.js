5dd2ba82705a9f309f3301d31f341859fb11f641
zephraph <jtbnnt@gmail.com>
2015-01-16 03:47:48 +0000

    Bumped meteor version to 1.2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'zephraph:namespace',
   summary: 'A simple javascript namespacing utility',
-  version: '1.1.0',
+  version: '1.2.0',
   git: 'https://github.com/zephraph/meteor-namespace.git'
 });
 
