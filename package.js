0a268e424e942853358c08d029fe80102006c513
Zephraph <zephraph@gmail.com>
2015-08-03 17:14:58 -0500

    Update package.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'zephraph:namespace',
   summary: 'A simple javascript namespacing utility',
-  version: '1.2.0',
+  version: '1.2.1',
   git: 'https://github.com/zephraph/meteor-namespace.git'
 });
 
